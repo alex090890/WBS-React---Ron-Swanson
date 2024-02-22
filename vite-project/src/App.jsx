d5e5890c31c9d38326181caf9b9d77a650bc2b2b
Oleksandr Lazurenko <alexprofteach@gmail.com>
2024-02-22 20:29:05 +0100

    Initial files

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <a href="/instructions.html"> instructions </a>
+      </h1>
+      <img
+        src="https://media.giphy.com/media/tSVnUxoWoHC/giphy.gif"
+        alt="ron"
+      />
+      <p>QUOTE</p>
+    </div>
+  );
+}
+
+export default App;
